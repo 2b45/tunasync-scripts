1612ff6442262b934bfe973a4edcb66db171790d
gmctl <actanble@gmail.com>
2020-12-24 16:42:47 +0800

    :tada:-增加iso

--- iso.sh
+++ iso.sh
@@ -9,7 +9,7 @@ BASE_PATH="${TUNASYNC_WORKING_DIR}"
 BASE_URL=${TUNASYNC_UPSTREAM_URL:-"ftp://ftp.bdp-app.topsec.con.cn"}
 export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
-lftp -e 'mirror -P 5 --verbose E:/DATAS/电子书 $BASE_PATH ' ftp://10.7.217.164
+lftp -e "mirror -P 5 -X Centos -X SLES* -X Windows --verbose E:/DATAS/公共资源/操作系统 $BASE_PATH" ftp://10.7.217.164
 
 echo "FTP finished"
 
